379538379e2b8840b1f6dddf307bc9b8ff69ff34
ponty <titi@mint12.(none)>
2012-02-08 11:18:53 +0100

    install

--- pavement.py
+++ pavement.py
@@ -12,6 +12,7 @@ try:
     from paved.util import *
     from paved.docs import *
     from paved.pycheck import *
+    from paved.pkg import *
     from sphinxcontrib import paverutils
     ALL_TASKS_LOADED = True
 except ImportError, e:
@@ -96,7 +97,15 @@ if ALL_TASKS_LOADED:
     
     
     @task
-    @needs('sloccount', 'cog', 'html', 'pdf', 'sdist', 'nose')
+    @needs(
+           'clean',
+           'sloccount', 
+           'cog', 
+           'html', 
+           'pdf', 
+           'sdist', 
+           'nose',
+           )
     def alltest():
         'all tasks to check'
         pass
