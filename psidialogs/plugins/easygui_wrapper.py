7a7e83af3948c76d6391a932f1612446570c3b8e
ponty <ponty@home>
2020-06-20 09:11:59 +0200

    easygui: @ansi_dialog removed

--- psidialogs/plugins/easygui_wrapper.py
+++ psidialogs/plugins/easygui_wrapper.py
@@ -1,4 +1,3 @@
-from psidialogs.unicodeutil import ansi_dialog
 from psidialogs.iplugin import IPlugin
 
 
@@ -14,17 +13,14 @@ class Backend(IPlugin):
     def backend_version(self):
         return self.easygui.egversion
 
-    @ansi_dialog
     def message(self, args):
         self.easygui.msgbox(msg=args["message"], title=args["title"])
 
-    @ansi_dialog
     def ask_string(self, args):
         return self.easygui.enterbox(
             default=args["default"], msg=args["message"], title=args["title"]
         )
 
-    @ansi_dialog
     def ask_file(self, args):
         if args["save"]:
             return self.easygui.filesavebox(
@@ -35,36 +31,30 @@ class Backend(IPlugin):
                 default=args["default"], msg=args["message"], title=args["title"]
             )
 
-    @ansi_dialog
     def ask_folder(self, args):
         return self.easygui.diropenbox(
             default=args["default"], msg=args["message"], title=args["title"]
         )
 
-    @ansi_dialog
     def choice(self, args):
         return self.easygui.choicebox(
             msg=args["message"], title=args["title"], choices=args["choices"]
         )
 
-    @ansi_dialog
     def multi_choice(self, args):
         return self.easygui.multchoicebox(
             msg=args["message"], title=args["title"], choices=args["choices"]
         )
 
-    @ansi_dialog
     def text(self, args):
         self.easygui.textbox(
             text=args["text"], msg=args["message"], title=args["title"]
         )
 
-    @ansi_dialog
     def ask_yes_no(self, args):
         x = self.easygui.ynbox(msg=args["message"], title=args["title"])
         return bool(x)
 
-    @ansi_dialog
     def ask_ok_cancel(self, args):
         x = self.easygui.ynbox(
             msg=args["message"], title=args["title"], choices=("OK", "Cancel")
