93b9c59294955df11cd085e083d596f366d526f1
ponty <ponty@home>
2022-06-11 08:07:23 +0200

    py311

--- tests/vagrant/debian10.sh
+++ tests/vagrant/debian10.sh
@@ -6,14 +6,6 @@ sudo update-locale LANG=en_US.UTF-8 LANGUAGE=en.UTF-8
 # install python versions
 # sudo add-apt-repository --yes ppa:deadsnakes/ppa
 sudo apt-get update
-# sudo apt-get install -y python3.6-dev
-# sudo apt-get install -y python3.7-dev
-# sudo apt-get install -y python3.8-dev
-# sudo apt-get install -y python3-distutils
-# sudo apt-get install -y python3.9-dev
-# sudo apt-get install -y python3.9-distutils
-# sudo apt-get install -y python3.10-dev
-# sudo apt-get install -y python3.10-distutils
 
 # tools
 sudo apt-get install -y mc xvfb x11-utils scrot
--- tests/vagrant/ubuntu2004.sh
+++ tests/vagrant/ubuntu2004.sh
@@ -6,15 +6,22 @@ sudo update-locale LANG=en_US.UTF-8 LANGUAGE=en.UTF-8
 # install python versions
 sudo add-apt-repository --yes ppa:deadsnakes/ppa
 sudo apt-get update
+
 sudo apt-get install -y python3.8-dev
 sudo apt-get install -y python3.8-distutils
 sudo apt-get install -y python3.8-tk
+
 sudo apt-get install -y python3.9-dev
 sudo apt-get install -y python3.9-distutils
 sudo apt-get install -y python3.9-tk
+
 sudo apt-get install -y python3.10-dev
 sudo apt-get install -y python3.10-distutils
 sudo apt-get install -y python3.10-tk
+
+sudo apt-get install -y python3.11-dev
+sudo apt-get install -y python3.11-distutils
+sudo apt-get install -y python3.11-tk
 
 # tools
 sudo apt-get install -y mc xvfb x11-utils scrot
